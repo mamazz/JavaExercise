8f5a6ad4ff6769d3793b839e3ffbea50736195e6
mamazz <nurilove3@gmail.com>
2016-06-10 17:02:15 +0900

    Update KawiBawiBo.java

--- src/basic/KawiBawiBo.java
+++ src/basic/KawiBawiBo.java
@@ -5,25 +5,18 @@ import java.util.Scanner;
 /**
  * Created by jyheo on 2016-03-10.
  */
-public class KawiBawiBo {
-    public static boolean isKawiBawiBoSwitch(String str) {
+public class KawiBawiBo_Sol {
+    public static int getKawiBawiBoSwitch(String str) {
         switch (str) {
             case "가위":
+                return 3;
             case "바위":
+                return 2;
             case "보":
-                return true;
+                return 1;
             default:
-                return false;
+                return 0;
         }
-    }
-
-    public static boolean isKawiBawiBoIf(String str) {
-        if (str.equals("가위") || str.equals("바위") || str.equals("보"))
-            return true;
-        else
-            return false;
-        // this if statement can be simplified such as
-        // return (str.equals("가위") || str.equals("바위") || str.equals("보"));
     }
 
     public static void main(String args[]) {
@@ -31,22 +24,47 @@ public class KawiBawiBo {
 
         System.out.print("Bob >> ");
         String bob = scanner.next();
-        if (!isKawiBawiBoSwitch(bob)) {
+        int iBob = getKawiBawiBoSwitch(bob);
+        if (iBob == 0) {
             System.out.println("Input allowed only : 가위, 바위, 보");
             return;
         }
         System.out.print("Alice >> ");
         String alice = scanner.next();
-        if (!isKawiBawiBoIf(alice)) {
+        int iAlice = getKawiBawiBoSwitch(alice);
+        if (iAlice == 0) {
             System.out.println("Input allowed only : 가위, 바위, 보");
             return;
         }
 
         // TODO: determine who win using if/else
+        /*if(bob.equals("가위")){
+if(alice.equals("가위")){
+System.out.println("Tie");}
 
-        // output will be one of :
-        System.out.println("Alice win!");
-        System.out.println("Bob win!");
-        System.out.println("Tie!");
+else if(alice.equals("바위")){
+System.out.println("Alice win");}
+
+else{
+System.out.println("bob win");}
+
+}else if(bob.equals("바위")){
+if(alice.equals("가위")){
+System.out.println("bob win");}
+else if(alice.equals("바위")){
+System.out.println("Tie");}
+else{
+System.out.println("Alice win");
+}}}}*/
+        
+        int iDet = iBob - iAlice;
+        if (iDet == 0) {
+            System.out.println("Tie!");
+        } else if (iDet == -1 || iDet == 2) {
+            System.out.println("Bob win!");
+        } else { // -2 or 1
+            // output will be one of :
+            System.out.println("Alice win!");
+        }
     }
 }
