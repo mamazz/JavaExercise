f8b4229fe743739d866b6a5c8c755e141c4cbd10
mamazz <nurilove3@gmail.com>
2016-06-10 17:57:44 +0900

    Update MultiplicationTable.java

--- src/classes/MultiplicationTable.java
+++ src/classes/MultiplicationTable.java
@@ -38,10 +38,12 @@ public class MultiplicationTable {
     public static void main(String[] args) {
         MultiplicationTable mtable = new MultiplicationTable();
 
-        mtable.max_number = 19; // BAD practice!
-        mtable.setMaxNumber(19);
+        mtable.setMaxNumber(9);
 
-        mtable.setNumber(4);
+        for(int i=2;i<=9;i++)
+        {
+        mtable.setNumber(i);
         mtable.printTable();
+         }
     }
 }
